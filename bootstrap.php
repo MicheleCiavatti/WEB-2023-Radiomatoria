2b2b2ea9649d9c357ab18ff95f6ff0c3b7abb9b6
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-12-26 14:08:53 +0100

    Added bootstrap.php

---
+++ bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+require_once("DB/database.php");
+//require_once("utils/functions.php");
+$dbh = new DatabaseHelper("localhost", "root", "", "longlight", 3306);
+//define("UPLOAD_DIR", "./upload/")
+?>
