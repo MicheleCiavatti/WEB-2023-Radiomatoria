21640b5252a4cd8d13e04aa501e6fc5c1f5fa485
AlessandroCacciaguerra <113693211+AlessandroCacciaguerra@users.noreply.github.com>
2024-01-04 14:33:47 +0100

    Update notifiche.js
    Shrunk javascript in favor of php

--- js/notifiche.js
+++ js/notifiche.js
@@ -1,13 +1,3 @@
 function count() {
     document.getElementById("notifications_total").innerHTML += document.getElementById("notifications_list").childElementCount;
 }
-
-function hide() {
-    document.getElementsByName("friend_accept").hidden = true;
-    document.getElementsByClassName("req_ami_button").hidden = false;
-}
-
-function write() {
-    document.getElementsByClassName("req_ami_text").innerHTML = "ti ha inviato una richiesta di amicizia";
-    document.getElementsByClassName("ref_ami_text").innerHTML = "ha rifiutato la tua richiesta di amicizia";
-}
