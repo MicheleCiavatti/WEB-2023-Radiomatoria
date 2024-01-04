274b346a69fd62f2316e366c756d305fb332bb87
AlessandroCacciaguerra <113693211+AlessandroCacciaguerra@users.noreply.github.com>
2024-01-04 15:34:48 +0100

    Update notifiche.php
    Fixed errors in php

--- php/notifiche.php
+++ php/notifiche.php
@@ -1,41 +1,34 @@
 <?php
     function removeNotification($nid) {
-        $stmt = $this->db->prepare("SELECT MotivoNotifica FROM Notifiche WHERE NrNotifica = ?");
-        $stmt->bind_param('s', $nid);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        if($result == "req_ami") {
-            $stmt = $this->db->prepare("SELECT MittenteNotifica FROM Notifiche WHERE NrNotifica = ?");
-            $stmt->bind_param('s', $nid);
-            $stmt->execute();
-            $result = $stmt->get_result();
-            refuseFriend($result);
-        }
-        $stmt = $this->db->prepare("DELETE FROM Notifiche WHERE NrNotifica = ?");
+        $stmt = $this->db->prepare("DELETE FROM NOTIFICA, CAUSA, RICEZIONE WHERE IdNotifica = ?");
         $stmt->bind_param('s', $nid);
         $stmt->execute();
     }
 
-    function outcomeNotification($senderid, $outcome) {
-        $stmt = $this->db->prepare("INSERT INTO Notifiche (IdNotifica, MittenteNotifica, MotivoNotifica, RiceventeNotifica) VALUES (?, ?, ?, ?)");
+    function outcomeNotification($nid, $senderid, $outcome) {
+        removeNotification($nid);
+        $stmt = $this->db->prepare("INSERT INTO NOTIFICA (IdNotifica, TestoNotifica, MittenteNotifica, Richiesta) VALUES (?, ?, ?, ?)");
         do {
             $nid = uniqid();
-            $idcheck = $this->db->prepare("SELECT IdNotifica FROM Notifiche WHERE IdNotifica = ?");
+            $idcheck = $this->db->prepare("SELECT IdNotifica FROM NOTIFICA WHERE IdNotifica = ?");
             $idcheck->bind_param('s', $nid);
             $idcheck->execute();
             $result = $idcheck->get_result();
         } while ($result == $nid);
-        $stmt->bind_param('ssss', $nid, $_SESSION['uid'], $outcome, $senderid);
+        $stmt->bind_param('sssi', $nid, $outcome, $_SESSION['uid'], false);
+        $stmt->execute();
+
+        $stmt = $this->db->prepare("INSERT INTO CAUSA (IdNotifica, NomeUtente) VALUES (?, ?)");
+        $stmt->bind_param('ss', $nid, $_SESSION['uid']);
+        $stmt->execute();
+
+        $stmt = $this->db->prepare("INSERT INTO RICEZIONE (IdNotifica, NomeUtente) VALUES (?, ?)");
+        $stmt->bind_param('ss', $nid, $senderid);
         $stmt->execute();
     }
 
-    function outcomeFriend($nid, $senderid, $outcome) {
-        outcomeNotification($senderid, $outcome);
-        removeNotification($nid);
-    }
-
     function addFriend($nid, $senderid) {
-        outcomeFriend($nid, $senderid, "acc_ami");
+        outcomeNotification($nid, $senderid, "ha accettato la tua richiesta di amicizia");
         $stmt = $this->db->prepare("INSERT INTO AMICIZIA VALUES (? ?)");
         $stmt->bind_param('ss', $_SESSION['uid'], $senderid);
         $stmt->execute();
