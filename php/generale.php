d912aa41d0d344b9a272d96d94dc21eeaf05fb06
AlessandroCacciaguerra <113693211+AlessandroCacciaguerra@users.noreply.github.com>
2024-01-17 22:49:44 +0100

    Fixed generale.php

--- php/generale.php
+++ php/generale.php
@@ -1,11 +1,35 @@
 <?php
 
     public function accessProfile($username) {
-        $stmt->db->prepare("SELECT UTENTE.*, BANDA.frequenza AS frequenze, (DISPONIBILITA.OraInizio, DISPONIBILITA.OraFine) AS orari, AMICIZIA.NomeAmico AS amici, FOLLOW.NomeSeguito AS seguiti, BLOCCO.NomeBloccato AS bloccati
-        FROM UTENTE, AMICIZIA, FOLLOW, BLOCCO, BANDA, DISPONIBILITA WHERE UTENTE.NomeUtente = ? AND AMICIZIA.NomeUtente = ? AND FOLLOW.NomeUtente = ? AND BLOCCO.NomeUtente = ? AND BANDA.NomeUtente = ? AND DISPONIBILITA.NomeUtente = ?");
-        $stmt->bind_param('ssssss', $username, $username, $username, $username, $username, $username);
+        $stmt->db->prepare("SELECT UTENTE.* FROM UTENTE WHERE UTENTE.NomeUtente = ?");
+        $stmt->bind_param('s', $username);
         $stmt->execute();
         $utente = $stmt->get_result();
+
+        $stmt->db->prepare("SELECT BANDA.frequenza FROM BANDA WHERE BANDA.NomeUtente = ?");
+        $stmt->bind_param('s', $username);
+        $stmt->execute();
+        $frequenze = $stmt->get_result();
+
+        $stmt->db->prepare("SELECT DISPONIBILITA.OraInizio, DISPONIBILITA.OraFine FROM DISPONIBILITA WHERE DISPONIBILITA.NomeUtente = ?");
+        $stmt->bind_param('s', $username);
+        $stmt->execute();
+        $orari = $stmt->get_result();
+
+        $stmt->db->prepare("SELECT AMICIZIA.NomeAmico FROM AMICIZIA WHERE AMICIZIA.NomeUtente = ?");
+        $stmt->bind_param('s', $username);
+        $stmt->execute();
+        $amici = $stmt->get_result();
+
+        $stmt->db->prepare("SELECT FOLLOW.NomeSeguito FROM FOLLOW WHERE FOLLOW.NomeUtente = ?");
+        $stmt->bind_param('s', $username);
+        $stmt->execute();
+        $seguiti = $stmt->get_result();
+
+        $stmt->db->prepare("SELECT BLOCCO.NomeBloccato FROM BLOCCO WHERE BLOCCO.NomeUtente = ?");
+        $stmt->bind_param('s', $username);
+        $stmt->execute();
+        $bloccati = $stmt->get_result();
         switchToProfile();
     }
 
