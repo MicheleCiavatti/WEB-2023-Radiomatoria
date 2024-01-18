84b9cd52ea10442fcfece144e5aa2427347eacac
AlessandroCacciaguerra <113693211+AlessandroCacciaguerra@users.noreply.github.com>
2024-01-18 15:10:21 +0100

    Update base.php

--- template/base.php
+++ template/base.php
@@ -10,106 +10,141 @@
             <h1>Long Light</h1>
         </header>
         <aside>
-        <?php 
-        session_start();
-        if(isset($_SESSION['uid']) && isset($_SESSION['img'])):
-        ?>
-        <img src="<?php echo $_SESSION['img']; ?>" alt=""/>
-        <p><?php echo $_SESSION['uid']; ?></p>
-        <?php endif; ?>
+            <?php 
+                session_start();
+                if(cookiesSet()):
+            ?>
+                <img id="FotoProfilo" src="<?php echo readCookie('FotoProfilo') ?>" alt=""/>
+                <button id="NomeUtente" onclick="accessProfile(<?php echo readCookie('NomeUtente'); ?>)"><?php echo readCookie('NomeUtente'); ?></button>
+            <?php endif; ?>
         </aside>
         <nav>
             <ul>
-                <li><a href="#">Profilo</a></li>
-                <li><a href="index.html">Home page</a></li>
-                <li><a href="guida.html">Guida</a></li>
-                <li><a href="create.html">Crea account</a></li>
-                <li>
-                    <?php if (isset($_SESSION['uid'])): ?>
-                        <a href="includes/logout.inc.php">Logout</a>
-                    <?php else: ?>
-                        <a href="login.html">Login</a>
-                    <?php endif; ?>
-                </li>
-                <li><a href="notifiche.html">Notifiche</a></li>
+                <?php if(cookiesSet()): ?>
+                    <li id="pag_profilo"><a href="accessProfile(<?php echo readCookie('NomeUtente'); ?>)">Profilo</a></li>
+                <?php endif; ?>
+                <li id="pag_principale"><a href="index.html">Home page</a></li>
+                <li id="pag_guida"><a href="guida.html">Guida</a></li>
+                <?php if(cookiesSet()): ?>
+                    <li id="pag_notifiche"><a href="notifiche.html">Notifiche</a></li>
+                    <li id="pag_uscita"><a href="includes/logout.inc.php">Logout</a></li>
+                <?php else: ?>
+                    <li id="pag_creazione"><a href="create.html">Crea account</a></li>
+                    <li id="pag_accesso"><a href="login.html">Login</a></li>
+                <?php endif; ?>
             </ul>
         </nav>
         <main>
-            <section>
-                <header>
-                <form action="sort_post.php" method="post" name="sort_form">
-                    <label for="sort">Ordina per</label>
-                    <select name="sort" id="sort" onchange="this.form.submit()">
-                        <option value="" selected>Seleziona</option>
-                        <option value="data">Data</option>
-                        <option value="like">Like</option>
-                        <option value="comm">Commenti</option>
-                    </select>
-                </form>
-                <button id="add_post_button" class="access_required" onclick="mostraFormPost()">Aggiungi post</button>
-                <form action="add_post.php" method="post" name="add_post_form">
-                    <table>
-                        <tr>
-                            <td><label for="post_img">Inserisci immagine (opzionale)</label></td>
-                            <td><input type="image" name="post_img" id="post_img" alt=""/></td>
-                        </tr>
-                        <tr>
-                            <td><label for="post_text">Inserisci testo</label></td>
-                            <td><textarea name="post_text" id="post_text" required></textarea></td>
-                        </tr>
-                        <tr>
-                            <td><input type="reset" value="Annulla"/></td>
-                            <td><input type="submit" value="Scrivi"/></td>
-                        </tr>
-                    </table>
-                </form>
+            <header>
+                <section>
+                    <form action="selectPostHome.php" method="post" name="select_form_home">
+                        <?php if(cookiesSet()): ?>
+                            <label for="origin">Seleziona post in base all'autore</label>
+                            <select name="origin" id="origin">
+                                <option value="all" selected>Tutti</option>
+                                <option value="strangers" >Sconosciuti</option>
+                                <option value="friends">Amici</option>
+                                <option value="followed">Seguiti</option>
+                                <option value="mine">Me stesso</option>
+                            </select>
+                        <?php endif; ?>
+                        <label for="sort">Ordina per</label>
+                        <select name="sort" id="sort" onchange="this.form.submit()">
+                            <option value="" selected>Seleziona</option>
+                            <option value="data">Data</option>
+                            <option value="like">Like</option>
+                            <option value="comm">Commenti</option>
+                        </select>
+                        <label for="order">In ordine decrescente</label>
+                        <input type="checkbox" name="order" id="order" checked/>
+                    </form>
+                    <?php if(cookiesSet()): ?>
+                        <button id="add_post_button" class="access_required" onclick="mostraFormPost()">Aggiungi post</button>
+                        <form action="addPost.php" method="post" name="add_post_form">
+                            <table>
+                                <tr>
+                                    <td><label for="post_img">Inserisci immagine (opzionale)</label></td>
+                                    <td><input type="image" name="post_img" id="post_img" alt=""/></td>
+                                </tr>
+                                <tr>
+                                    <td><label for="post_text">Inserisci testo</label></td>
+                                    <td><textarea name="post_text" id="post_text" required></textarea></td>
+                                </tr>
+                                <tr>
+                                    <td><input type="reset" value="Annulla"/></td>
+                                    <td><input type="submit" value="Scrivi"/></td>
+                                </tr>
+                            </table>
+                        </form>
+                    <?php endif; ?>
+                </section>
             </header>
-            <main>
+            <article>
                 <ul>
-                    <?php foreach($templateParams["post"] as $post): ?>
+                    <?php foreach($post_list as $post): ?>
                     <li>
                         <table>
                             <tr>
-                                <td><button onclick="accessProfile()"></button><?php echo $post["UserPost"]; ?></td>
+                                <td><button onclick="accessProfile(<?php echo $post['Creatore']; ?>)"></button><?php echo $post["Creatore"]; ?></td>
                                 <td><?php echo $post["DataPost"]; ?></td>
-                                <td><button onclick="removePost($post['NrPost'], $post['UserPost'])" class="access_required">Rimuovi</button></td>
+                                <?php if ($post["Creatore"] == readCookie('NomeUtente')): ?>
+                                    <td><button onclick="removePost($post['NrPost'])" class="access_required">Rimuovi</button></td>
+                                <?php endif; ?>
                             </tr>
                             <tr><td><?php echo $post["TestoPost"]; ?></td></tr>
-                            <tr><td><img src="$post['ImmaginePost']" alt=""/></td></tr>
+                            <tr><td><img src="<?php echo $post['ImmaginePost']; ?>" alt=""/></td></tr>
                             <tr>
                                 <td><?php echo $post["LikePost"]; ?></td>
-                                <td><button name="$post['NrPost']_like_button" class="preference_button" onclick="like($post['NrPost'])">Like</button></td>  <!-- aggiunge un like al post (se c'era già un dislike lo rimuove) e cambia il testo in "Liked" ed il colore in ciano; se c'è già un like si rimuove (toglie il like, torna come prima e non fa altro) -->
+                                <td><button name="<?php echo $post['NrPost']; ?>_like_button" class="preference_button" onclick="<?php if(cookiesSet()): ?>like(<?php echo $post['NrPost']; ?>)<?php endif; ?>">Like</button></td>
                                 <td><?php echo $post["DislikePost"]; ?></td>
-                                <td><button name="$post['NrPost']_dislike_button" class="preference_button" onclick="dislike($post['NrPost'])">Dislike</button></td>  <!-- aggiunge un dislike al post (se c'era già un like lo rimuove) e cambia il testo in "Disliked" ed il colore in magenta; se c'è già un dislike si rimuove -->
-                                <td><button id="add_comment_button" class="access_required" onclick="mostraFormCommenti($post['NrPost'], $post['UserPost'], $post['DataPost'], '')">Commenta</button></td>
+                                <td><button name="<?php echo $post['NrPost']; ?>_dislike_button" class="preference_button" onclick="<?php if(cookiesSet()): ?>dislike(<?php echo $post['NrPost']; ?>)<?php endif; ?>">Dislike</button></td>
+                                <?php if(cookiesSet()): ?>
+                                    <td><button id="add_comment_button" class="access_required" onclick="mostraFormCommenti($post['NrPost'], $post['Creatore'], $post['DataPost'], '')">Commenta</button></td>
+                                <?php endif; ?>
                             </tr>
-                            <tr><td><button onclick="mostraCommentiPost($post['NrPost'])">Mostra commenti</button></td></tr>
+                            <tr><td><button onclick="mostraCommentiPost(<?php echo $post['NrPost']; ?>)">Mostra commenti</button></td></tr>
                         </table>
-                        <ul id="$post['NrPost']_comment_list">
-                            <?php foreach($templateparams["commenti"] as $commento): ?>
+                        <ul id="<?php echo $post['NrPost']; ?>_comment_list">
+                            <?php 
+                                $query = "SELECT COMMENTO.*, COUNT(CASE WHEN INTERAZIONE.Tipo THEN 1 END) AS LikeCommento, COUNT(CASE WHEN NOT INTERAZIONE.Tipo THEN 1 END) AS DislikeCommento,
+                                SCRITTURA.NomeUtente AS UserCommento
+                                FROM (CONTENUTO INNER JOIN COMMENTO ON CONTENUTO.NrCommento = COMMENTO.NrCommento LEFT JOIN INTERAZIONE ON COMMENTO.NrCommento = INTERAZIONE.ElementId)
+                                INNER JOIN SCRITTURA ON CONTENUTO.NrCommento = SCRITTURA.NrCommento WHERE CONTENUTO.NrPost = ? ORDER BY COMMENTO.DataCommento DESC";
+                                $stmt = $this->db->prepare($query);
+                                $stmt->bind_param('ssi', $post['NrPost']);
+                                $stmt->execute();
+                                $commenti = $stmt->get_result();
+                                foreach($commenti as $commento):
+                            ?>
                                 <li>
                                     <table>
                                         <tr>
-                                            <td><button onclick="accessProfile($commento['UserCommento'])"><?php echo $commento["UserCommento"]; ?></button></td>
+                                            <td><button onclick="accessProfile(<?php echo $commento['UserCommento']; ?>)"><?php echo $commento["UserCommento"]; ?></button></td>
                                             <td><?php echo $commento["DataCommento"]; ?></td>
-                                            <td><button onclick="removeComment($commento['NrCommento'], $commento['UserCommento'])" class="access_required">Rimuovi</button></td>
-                                            <!-- removeComment non funziona se l'utente registrato non è l'autore del commento -->
+                                            <?php if($commento['Creatore'] == readCookie('NomeUtente')): ?>
+                                                <td><button onclick="removeComment(<?php echo $commento['NrCommento']; ?>)" class="access_required">Rimuovi</button></td>
+                                            <?php endif; ?>
                                         </tr>
                                         <tr><td><?php echo $commento["TestoCommento"]; ?></td></tr>
-                                        <tr><td><img src="$commento['ImmagineCommento']" alt=""/></td></tr>
+                                        <tr><td><img src="<?php echo $commento['ImmagineCommento']; ?>" alt=""/></td></tr>
                                         <tr>
                                             <td><?php echo $commento["LikeCommento"]; ?></td>
-                                            <td><button id="$commento['NrCommento']_like_button" class="preference_button" onclick="like($commento['NrCommento'])">Like</button></td>
+                                            <td><button id="<?php echo $commento['NrCommento']; ?>_like_button" class="preference_button"
+                                            onclick="<?php if(cookiesSet()): ?>like(<?php echo $commento['NrCommento']; ?>)<?php endif; ?>">Like</button></td>
                                             <td><?php echo $commento["DislikeCommento"]; ?></td>
-                                            <td><button id="$commento['NrCommento']_dislike_button" class="preference_button" onclick="dislike($commento['NrCommento'])">Dislike</button></td>
-                                            <td><button onclick="mostraFormCommenti($post['NrPost'], $post['UserPost'], $post['DataPost'], '@' + $commento['UserCommento'])">Rispondi</button></td>
+                                            <td><button id="<?php echo $commento['NrCommento']; ?>_dislike_button" class="preference_button"
+                                            onclick="<?php if(cookiesSet()): ?>dislike(<?php echo $commento['NrCommento']; ?>)<?php endif; ?>">Dislike</button></td>
+                                            <?php if(cookiesSet()): ?>
+                                                <td><button onclick="mostraFormCommenti($post['NrPost'], $post['Creatore'], $post['DataPost'], '@' + $commento['Creatore'])">Rispondi</button></td>
+                                            <?php endif; ?>
                                         </tr>
                                     </table>
                                 </li>
                             <?php endforeach; ?>
                         </ul>
-                        <form action="add_comment.php" method="post" name="add_comment_form">
-                            <table>
+                        <?php if(cookiesSet()): ?>
+                            <form action="addComment.php" method="post" name="add_comment_form">
+                                <table>
                                     <tr>
                                         <td id="comment_post_info"></td>
                                         <td><input type="hidden" name="post_id_input" id="post_id_input"/></td>
@@ -128,13 +163,14 @@
                                     </tr>
                                 </table>
                             </form>
+                        <?php endif; ?>
                         </li>
-                    <?php endforeach; ?>
-                    </ul>
-                </main>
-            </section>
+                    <?php endforeach;
+                    decorate($element_id_like, $element_id_dislike); ?>
+                </ul>
+            </article>
         </main>
-        <script src="js/generale.js" type="text/javascript"></script>
         <script src="js/index.js" type="text/javascript"></script>
+        <script src="js/generale.js" type="text/javascript"></script>
     </body>
 </html>
