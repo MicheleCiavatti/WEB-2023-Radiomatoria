f377d9d69c705da5f1f59a0cceefecced875c0b1
AlessandroCacciaguerra <113693211+AlessandroCacciaguerra@users.noreply.github.com>
2024-01-20 20:02:58 +0100

    Delete js/generale.js

--- js/generale.js
+++
@@ -1,4 +0,0 @@
-function accessProfile(username) {
-    window.location.href = "profile.php";
-    document.getElementsByTagName("body").onload("<?php $data = profileAccess('" + username + "'); $utente = $data[0]; $frequenze = $data[1]; $orari = $data[2]; $amici = $data[3]; $seguiti = $data[4]; $bloccati = $data[5]; ?>");
-}
