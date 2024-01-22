c4e9eda28f98f060cd03553a73d34584f9ddc657
AlessandroCacciaguerra <113693211+AlessandroCacciaguerra@users.noreply.github.com>
2024-01-22 21:33:10 +0100

    Split off non-AJAX function

---
+++ functions/list_notifications.php
@@ -0,0 +1,21 @@
+<?php
+    session_start();
+    require_once "../classes/dbh.classes.php";
+
+    function list_notifications() {
+        $dbh = new Dbh;
+        $stmt = $dbh->connect()->prepare("SELECT * FROM NOTIFICHE WHERE NOTIFICHE.Ricevente = ? AND NOTIFICHE.Lettura = ?");
+        if($stmt === false) {
+            error_log("Errore nella preparazione della query SELECT.");
+        }
+        if(!$stmt->execute(array($_COOKIE['NomeUtente'], false))) {
+            error_log("Errore nell'esecuzione della query SELECT: " . print_r($idcheck->errorInfo(), true));
+        }
+        $notifiche_non_lette = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        if(!$stmt->execute(array($_COOKIE['NomeUtente'], true))) {
+            error_log("Errore nell'esecuzione della query SELECT: " . print_r($idcheck->errorInfo(), true));
+        }
+        $notifiche_lette = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return array($notifiche_non_lette, $notifiche_lette);
+    }
+?>
