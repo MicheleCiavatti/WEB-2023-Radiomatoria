8b1df002d61c8f6934ebf7e09d36f9dac326a06b
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2024-01-23 13:47:55 +0100

    Fixed removal of frequencies

--- php/functions/removeFrequency.php
+++ php/functions/removeFrequency.php
@@ -4,17 +4,20 @@ require_once "../classes/dbh.classes.php";
 $dbh = new Dbh;
 
 if (isset($_GET['username']) && isset($_GET['f_to_remove'])) {
-    error_log("Variabili arrivate: " . $_GET['username'] . " e " . $_GET['f_to_remove']);
+    $f = $_GET['f_to_remove'];
+    error_log("Variabili arrivate: " . $_GET['username'] . " e " . $f);
     $s = $dbh->connect()->prepare(
         'DELETE
          FROM BANDE
-         WHERE NomeUtente = ? AND MHz = ?;'
+         WHERE NomeUtente = ? AND MHz BETWEEN ? AND ?;'
     );
+    $lower = $f - 0.01;
+    $upper = $f + 0.01;
     if ($s == false) {
         error_log("Errore nella preparazione della query DELETE.");
         exit;
     }
-    if(!$s->execute(array($_GET['username'], floatval($_GET['f_to_remove'])))) {
+    if(!$s->execute(array($_GET['username'], $lower, $upper))) {
         error_log("Errore nell'esecuzione della query DELETE: " . print_r($s->errorInfo(), true));
     } 
 } else {
