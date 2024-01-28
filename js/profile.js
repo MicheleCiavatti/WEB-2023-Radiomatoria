1b00f9ed11cf4977e344bcb8528d47a9c619b8ac
AlessandroCacciaguerra <113693211+AlessandroCacciaguerra@users.noreply.github.com>
2024-01-28 14:46:28 +0100

    Add files via upload

--- js/profile.js
+++ js/profile.js
@@ -1,3 +1,8 @@
+const intestazione = document.getElementById("intestazione_orari");
+const riga1 = document.getElementById("riga_orari_mattina");
+const riga2 = document.getElementById("riga_orari_sera");
+const removeTimeIntervalButtons = document.getElementsByClassName('remove_timeslot_buttons');
+
 document.addEventListener('DOMContentLoaded', function() {
     let username = document.getElementById('session_user_name');
     let other = document.getElementById('profile_name').innerHTML;
@@ -20,7 +25,6 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
     /* Handling remove time interval buttons */
-    let removeTimeIntervalButtons = document.getElementsByClassName('remove_timeslot_buttons');
     if (removeTimeIntervalButtons.length > 0) {
         for (i = 0; i < removeTimeIntervalButtons.length; i++) {
             let button = removeTimeIntervalButtons[i];
@@ -29,7 +33,34 @@ document.addEventListener('DOMContentLoaded', function() {
             let start = times[0].trim();
             let end = times[1].trim();
             button.addEventListener('click', function() {removeTimeInterval(username, start, end, id) });
+
+            /* Aesthetics */
+            let oraInizio = start.slice(0,2);
+            let oraFine = end.slice(0,2);
+            for(item of intestazione.children) {
+                if((oraInizio <= parseInt(item.innerHTML)
+                && oraFine >= parseInt(item.innerHTML)
+                && oraInizio <= oraFine) || (oraInizio > oraFine
+                && (oraInizio <= parseInt(item.innerHTML) || oraFine >= parseInt(item.innerHTML)))) {
+                    riga1.children.item(parseInt(item.innerHTML)).style.background = "green";
+                }
+                if((oraInizio <= parseInt(item.innerHTML) + 12
+                && oraFine >= parseInt(item.innerHTML) + 12
+                && oraInizio <= oraFine) || (oraInizio > oraFine
+                && (oraInizio <= parseInt(item.innerHTML) + 12 || oraFine >= parseInt(item.innerHTML) + 12))) {
+                    riga2.children.item(parseInt(item.innerHTML)).style.background = "green";
+                }
+            }
         }
+    }
+    let tempo_corrente = new Date();
+    let ora_corrente = tempo_corrente.getHours();
+    if(ora_corrente<=12) {
+        document.getElementById("intestazione_orari").children.item(ora_corrente).style.color = "green";
+        riga1.children.item(0).style.color = "green";
+    } else {
+        document.getElementById("intestazione_orari").children.item(ora_corrente-12).style.color = "green";
+        riga2.children.item(0).style.color = "green";
     }
 });
 
@@ -59,9 +90,41 @@ function removeTimeInterval(username, start, end, id) {
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
-            console.log('Frequenza rimossa con successo dal server');
+            let oraInizio = start.slice(0,2);
+            let oraFine = end.slice(0,2);
+            for (i = 0; i < removeTimeIntervalButtons.length; i++) {
+                if((removeTimeIntervalButtons[i].closest('li').innerHTML.split('<')[0].split(' - '))[1].trim().slice(0,2) == oraInizio) {
+                    let overlapInizio = true;
+                    if(typeof overlapFine !== 'undefined') { break; }
+                }
+                if((removeTimeIntervalButtons[i].closest('li').innerHTML.split('<')[0].split(' - '))[0].trim().slice(0,2) == oraFine) {
+                    let overlapFine = true;
+                    if(typeof overlapInizio !== 'undefined') { break; }
+                }
+            }
+            for(item of intestazione.children) {
+                if((oraInizio <= parseInt(item.innerHTML)
+                && oraFine >= parseInt(item.innerHTML)
+                && oraInizio < oraFine) || (oraInizio > oraFine
+                && (oraInizio <= parseInt(item.innerHTML) || oraFine >= parseInt(item.innerHTML)))) {
+                    if(!((typeof overlapInizio !== 'undefined' && parseInt(item.innerHTML) == oraInizio) || 
+                    (typeof overlapFine !== 'undefined' && parseInt(item.innerHTML) == oraFine))) {
+                        riga1.children.item(parseInt(item.innerHTML)).style.background = "none";
+                    }
+                }
+                if((oraInizio <= parseInt(item.innerHTML) + 12
+                && oraFine >= parseInt(item.innerHTML) + 12
+                && oraInizio < oraFine) || (oraInizio > oraFine
+                && (oraInizio <= parseInt(item.innerHTML) + 12 || oraFine >= parseInt(item.innerHTML) + 12))) {
+                    if(!((typeof overlapInizio !== 'undefined' && parseInt(item.innerHTML) == oraInizio) || 
+                    (typeof overlapFine !== 'undefined' && parseInt(item.innerHTML) == oraFine))) {
+                        riga2.children.item(parseInt(item.innerHTML)).style.background = "none";
+                    }
+                }
+            }
+            console.log('Fascia oraria rimossa con successo dal server');
         } else if (xhr.readyState === 4 && xhr.status !== 200) {
-            console.error('Errore durante la rimozione della frequenza:', xhr.status);
+            console.error('Errore durante la rimozione della fascia oraria:', xhr.status);
         }
     };
     xhr.send();
@@ -98,3 +161,5 @@ function removeFollow(username, other) {
     };
     xhr.send();
 }
+
+//onclick="removeFrequency('<?= $f?>', '<?= $utente['NomeUtente']?>', '<?= '#f' . str_replace('.', '_', $f)?>')"
