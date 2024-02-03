3c8f245d3fb4670e97132413d8299e340572f310
AlessandroCacciaguerra <113693211+AlessandroCacciaguerra@users.noreply.github.com>
2024-02-03 22:01:49 +0100

    Add files via upload

--- php/includes/notificationsInfo.inc.php
+++ php/includes/notificationsInfo.inc.php
@@ -2,28 +2,42 @@
     session_start();
     require_once __DIR__ . "/../classes/dbh.classes.php";
 
-    function notificationAccess($username) {
+    function notificationAccess() {
         $dbh = new Dbh;
         $s = $dbh->connect()->prepare(
             'SELECT *
              FROM NOTIFICHE
-             WHERE Ricevente = ?;'
+             WHERE Ricevente = ? AND Lettura = ?;'
         );
-        if (!$s->execute(array($username))) {
+        if (!$s->execute(array($_SESSION['NomeUtente'], false))) {
             error_log("Errore nell'esecuzione della query SELECT: " . print_r($s->errorInfo(), true));
             exit();
         }
         $r = $s->fetchAll(PDO::FETCH_ASSOC);
-        $notifications = [];
+        $unread_notifications = [];
         foreach ($r as $row) {
-            $notifications[] = array('Ricevente' => $row['Ricevente'],
+            $unread_notifications[] = array('Ricevente' => $row['Ricevente'],
                                      'IdNotifica' => $row['IdNotifica'],
                                      'Mandante' => $row['Mandante'], 
                                      'TestoNotifica' => $row['TestoNotifica'], 
                                      'Richiesta' => $row['Richiesta'], 
                                      'Lettura' => $row['Lettura']);
         }
-        return $notifications;
+        if (!$s->execute(array($_SESSION['NomeUtente'], true))) {
+            error_log("Errore nell'esecuzione della query SELECT: " . print_r($s->errorInfo(), true));
+            exit();
+        }
+        $r = $s->fetchAll(PDO::FETCH_ASSOC);
+        $read_notifications = [];
+        foreach ($r as $row) {
+            $read_notifications[] = array('Ricevente' => $row['Ricevente'],
+                                     'IdNotifica' => $row['IdNotifica'],
+                                     'Mandante' => $row['Mandante'], 
+                                     'TestoNotifica' => $row['TestoNotifica'], 
+                                     'Richiesta' => $row['Richiesta'], 
+                                     'Lettura' => $row['Lettura']);
+        }
+        return array($unread_notifications, $read_notifications);
     }
 
     
