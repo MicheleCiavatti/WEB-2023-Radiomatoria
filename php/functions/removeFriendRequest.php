7f312ce5a197f2ad2bcfbc47744714c00de957ee
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2024-02-04 16:16:39 +0100

    minor refactoring

--- php/functions/removeFriendRequest.php
+++ php/functions/removeFriendRequest.php
@@ -1,22 +1,10 @@
 <?php
-require_once "../classes/dbh.classes.php";
-$dbh = new Dbh;
+require_once "Notify.php";
 
 if (isset($_GET['username']) && isset($_GET['other'])) {
-    $user = $_GET['username'];
-    $other = $_GET['other'];
-    $s = $dbh->connect()->prepare(
-        'DELETE
-         FROM NOTIFICHE
-         WHERE Mandante = ? AND Ricevente = ? AND Richiesta = 1;'
-    );
-    if ($s == false) {
-        error_log("Errore nella preparazione della query DELETE.");
-        exit();
-    }
-    if(!$s->execute(array($user, $other))) {
-        error_log("Errore nell'esecuzione della query DELETE: " . print_r($s->errorInfo(), true));
-    }
+    $sender = $_GET['username'];
+    $receiver = $_GET['other'];
+    removeNotification($sender, $receiver, 0, 1);
 } else {
     error_log("Variabili non settate");
 }
