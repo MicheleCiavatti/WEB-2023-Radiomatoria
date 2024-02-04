87646dd99dacc685a050fb01861c11259bfbfed8
AlessandroCacciaguerra <113693211+AlessandroCacciaguerra@users.noreply.github.com>
2024-02-04 19:36:47 +0100

    Fixed $nid

--- php/functions/outcomeNotification.php
+++ php/functions/outcomeNotification.php
@@ -1,7 +1,7 @@
 <?php
         require_once "../classes/dbh.classes.php";
         session_start();
-        if (isset($_GET['username']) && isset($_GET['nid']) && isset($_GET['senderid']) && isset($_GET['outcome'])) {
+        if (isset($_GET['username']) && isset($_GET['senderid']) && isset($_GET['outcome'])) {
             $dbh = new Dbh;
             $idcheck = $dbh->connect()->prepare("SELECT COUNT(*) FROM NOTIFICHE WHERE IdNotifica = ?");
             if($idcheck === false) {
@@ -9,8 +9,8 @@
             }
             do {
                 $result = null;
-                $nid = hexdec(uniqid());
-                if(!$idcheck->execute(array($_GET['nid']))) {
+                $nid = rand(1,999);
+                if(!$idcheck->execute(array($nid))) {
                     error_log("Errore nell'esecuzione della query SELECT: " . print_r($idcheck->errorInfo(), true));
                 }
                 $result = $idcheck->fetch(PDO::FETCH_NUM);
@@ -19,7 +19,7 @@
             if($stmt === false) {
                 error_log("Errore nella preparazione della query INSERT.");
             }
-            if(!$stmt->execute(array($_GET['senderid'], $_GET['username'], $_GET['nid'], $_GET['outcome'], false, false))) {
+            if(!$stmt->execute(array($_GET['senderid'], $_GET['username'], $nid, $_GET['outcome'], false, false))) {
                 error_log("Errore nell'esecuzione della query INSERT: " . print_r($stmt->errorInfo(), true));
             }    
         }
