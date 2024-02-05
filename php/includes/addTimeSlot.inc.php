0afce98a6a801697169a4087af48243ed9e1369d
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2024-02-05 11:51:03 +0100

    Now end time must be > than start time in profile.php

--- php/includes/addTimeSlot.inc.php
+++ php/includes/addTimeSlot.inc.php
@@ -4,9 +4,9 @@ if (isset($_POST['orainizio']) && isset($_POST['orafine'])) {
     $start = $_POST['orainizio'];
     $end = $_POST['orafine'];
     $uid = $_SESSION['NomeUtente'];
-
-    if($start == $end) {
-        header('location: ../profile.php?id=' . $uid . '&error=start=time');
+    $stringHeader = "location: ../profile.php?id=" . $uid;
+    if($end <= $start) {
+        header($stringHeader . '&error=endbeforestart');
         exit();
     }
     
@@ -15,7 +15,7 @@ if (isset($_POST['orainizio']) && isset($_POST['orafine'])) {
     $stmt = $dbh->connect()->prepare("SELECT OraInizio, OraFine FROM DISPONIBILITA WHERE Utente = ?");
     if(!$stmt->execute(array($uid))) {
         $stmt = null;
-        header('location: ../../login.html?error=stmtfailed');
+        header($stringHeader . '&error=stmtfailed');
         exit();
     }
     $result = $stmt->fetchAll(PDO::FETCH_NUM);
@@ -28,7 +28,7 @@ if (isset($_POST['orainizio']) && isset($_POST['orafine'])) {
         if($interval[0] > $interval[1]) {
             $end2 += 12;
         }
-        if(($start < $interval[0] && $interval[0] < $end1) || ($interval[0] < $start && $start < $end2)
+        if (($start < $interval[0] && $interval[0] < $end1) || ($interval[0] < $start && $start < $end2)
         || $interval[0] == $start || $interval[1] == $end) {
             header('location: ../profile.php?id=' . $uid . '&error=overlap');
             exit();
