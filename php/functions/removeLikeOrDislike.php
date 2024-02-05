be7f7f5d1ec5247c6c0d279ec4316a8ce73a5167
AlessandroCacciaguerra <113693211+AlessandroCacciaguerra@users.noreply.github.com>
2024-02-05 21:51:45 +0100

    Created mechanisms for like and dislike

--- php/functions/removeLikeOrDislike.php
+++ php/functions/removeLikeOrDislike.php
@@ -4,13 +4,24 @@ require_once "../classes/dbh.classes.php";
 
 $dbh = new Dbh;
                             
-if (isset($_GET['author']) && isset($_GET['post_id']) && isset($_GET['creator']) && isset($_GET['comment_id'])) {
-    $stmt = $dbh->connect()->prepare("DELETE FROM INTERAZIONI WHERE Creatore = ? AND ElementIdPost = ? AND ElementCreator = ? AND ElementIdCommento = ?");
-    if($stmt == false) {
-        error_log("Errore nella preparazione della query DELETE.");
-        exit();
-    }
-    if(!$stmt->execute(array($_GET['author'], $_GET['post_id'], $_GET['creator'], $_GET['comment_id']))) {
-        error_log("Errore nell'esecuzione della query DELETE: " . print_r($stmt->errorInfo(), true));
+if (isset($_GET['author']) && isset($_GET['post_id']) && isset($_GET['creator'])) {
+    if(isset($_GET['comment_id']) && $_GET['comment_id'] > 0) {
+        $stmt = $dbh->connect()->prepare("DELETE FROM REAZIONI WHERE Creatore = ? AND ElementIdPost = ? AND ElementIdCommento = ? AND Reagente = ?");
+        if($stmt == false) {
+            error_log("Errore nella preparazione della query DELETE.");
+            exit();
+        }
+        if(!$stmt->execute(array($_GET['creator'], $_GET['post_id'], $_GET['comment_id'], $_GET['author']))) {
+            error_log("Errore nell'esecuzione della query DELETE: " . print_r($stmt->errorInfo(), true));
+        }
+    } else {
+        $stmt = $dbh->connect()->prepare("DELETE FROM INTERAZIONI WHERE Creatore = ? AND ElementId = ? AND Interagente = ?");
+        if($stmt == false) {
+            error_log("Errore nella preparazione della query DELETE.");
+            exit();
+        }
+        if(!$stmt->execute(array($_GET['creator'], $_GET['post_id'], $_GET['author']))) {
+            error_log("Errore nell'esecuzione della query DELETE: " . print_r($stmt->errorInfo(), true));
+        }
     }
 }
