4170c8a403e1f1372a66bf3a741d382d62f450ff
AlessandroCacciaguerra <113693211+AlessandroCacciaguerra@users.noreply.github.com>
2024-02-05 21:51:04 +0100

    Adjusted to ddl changes

--- php/functions/removeComment.php
+++ php/functions/removeComment.php
@@ -13,6 +13,15 @@ if (isset($_GET['pid']) && isset($_GET['creator']) && isset($_GET['cid'])) {
         error_log("Errore nell'esecuzione della query DELETE: " . print_r($stmt->errorInfo(), true));
         exit();
     }
+    $stmt = $dbh->connect()->prepare("DELETE FROM REAZIONI WHERE ElementIdPost = ? AND Creatore = ? AND ElementIdCommento = ?");
+    if($stmt == false) {
+        error_log("Errore nella preparazione della query DELETE.");
+        exit();
+    }
+    if(!$stmt->execute(array($_GET['pid'], $_GET['creator'], $_GET['cid']))) {
+        error_log("Errore nell'esecuzione della query DELETE: " . print_r($stmt->errorInfo(), true));
+        exit();
+    }
 } else {
     error_log("Variabili non settate");
 }
