48142e0bac785a72e88787dc87072175d5b84ed9
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2024-02-05 11:56:05 +0100

    Minor fix in time_slots

--- php/includes/addTimeSlot.inc.php
+++ php/includes/addTimeSlot.inc.php
@@ -20,33 +20,20 @@ if (isset($_POST['orainizio']) && isset($_POST['orafine'])) {
     }
     $result = $stmt->fetchAll(PDO::FETCH_NUM);
     $end1 = $end;
-    if($start > $end) {
-        $end1 += 12;
-    }
     foreach($result as $interval) {
         $end2 = $interval[1];
-        if($interval[0] > $interval[1]) {
-            $end2 += 12;
-        }
         if (($start < $interval[0] && $interval[0] < $end1) || ($interval[0] < $start && $start < $end2)
         || $interval[0] == $start || $interval[1] == $end) {
-            header('location: ../profile.php?id=' . $uid . '&error=overlap');
+            header($stringHeader . '&error=overlap');
             exit();
         }
     }
-    $stmt = $dbh->connect()->prepare("SELECT COUNT(*) FROM DISPONIBILITA WHERE OraInizio = ? AND Utente = ?");
-    if(!$stmt->execute(array($start, $uid))) {
-        $stmt = null;
-        header('location: ../profile.php?id = ' . $uid . '&error=stmtfailed');
-        exit();
-    }
-    $result = $stmt->fetch(PDO::FETCH_NUM);
     $stmt = $dbh->connect()->prepare("INSERT INTO DISPONIBILITA (OraInizio, OraFine, Utente) VALUES (?, ?, ?)");
     if(!$stmt->execute(array($start, $end, $uid))) {
         $stmt = null;
-        header('location: ../profile.php?id = ' . $uid . '&error=stmtfailed');
+        header($stringHeader . '&error=stmtfailed');
         exit();
     }
 
-    header('location: ../profile.php?id=' . $uid .'&error=none');
+    header($stringHeader);
 }
