08b2a18161bcd7f1d1bf557d3eb409cc2b6d83a8
AlessandroCacciaguerra <113693211+AlessandroCacciaguerra@users.noreply.github.com>
2024-02-06 17:58:27 +0100

    Update outcomeNotification.php

--- php/functions/outcomeNotification.php
+++ php/functions/outcomeNotification.php
@@ -3,18 +3,16 @@
         session_start();
         if (isset($_GET['username']) && isset($_GET['senderid']) && isset($_GET['outcome'])) {
             $dbh = new Dbh;
-            $idcheck = $dbh->connect()->prepare("SELECT COUNT(*) FROM NOTIFICHE WHERE IdNotifica = ?");
+            $idcheck = $dbh->connect()->prepare("SELECT * FROM NOTIFICHE WHERE IdNotifica = ?");
             if($idcheck === false) {
                 error_log("Errore nella preparazione della query SELECT.");
             }
             do {
-                $result = null;
                 $nid = rand(1,999);
                 if(!$idcheck->execute(array($nid))) {
                     error_log("Errore nell'esecuzione della query SELECT: " . print_r($idcheck->errorInfo(), true));
                 }
-                $result = $idcheck->fetch(PDO::FETCH_NUM);
-            } while ($result[0] > 0);
+            } while ($idcheck->rowCount() > 0);
             $stmt = $dbh->connect()->prepare("INSERT INTO NOTIFICHE (Ricevente, Mandante, IdNotifica, TestoNotifica, Richiesta, Lettura) VALUES (?, ?, ?, ?, ?, ?)");
             if($stmt === false) {
                 error_log("Errore nella preparazione della query INSERT.");
