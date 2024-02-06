75faea08eb1eef7f8b99c77342cf2ac4f4c1b813
AlessandroCacciaguerra <113693211+AlessandroCacciaguerra@users.noreply.github.com>
2024-02-06 11:42:39 +0100

    Delete functions directory

--- functions/profile/friendRequest.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once "../classes/dbh.classes.php";
-require_once "../profile.php";
-    
-if(isset($_COOKIE['NomeUtente']) && isset($_POST['amico'])) {
-    notify("ti ha aggiunto alla sua lista di seguiti", $_POST['amico'], true);
-}
